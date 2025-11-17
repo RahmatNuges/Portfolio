45daac1190da5da0d97d7178c8bb38b6fa01587c
RahmatNuges <rahmatnugess@gmail.com>
2025-11-17 15:06:28 +0700

    webp

--- src/components/PortfolioList.tsx
+++ src/components/PortfolioList.tsx
@@ -12,35 +12,35 @@ export const portfolioItems: PortfolioItem[] = [
     description: 'Tunjukkan profesionalisme bisnis Anda dengan website company profile yang elegan dan mudah diakses. Cocok untuk membangun kepercayaan klien baru!',
     route: 'https://arsiteku.vercel.app/',
     category: 'Company Profile',
-    screenshot: '/portfolio/Arsiteku.png',
+    screenshot: '/portfolio/Arsiteku.webp',
   },
   {
     title: 'Website Villa Management',
     description: 'Bangun website management villa anda yang mempermudah pelanggan anda memilih dan booking villa anda!!',
     route: 'https://villa-lime.vercel.app/',
     category: 'Villa Management',
-    screenshot: '/portfolio/Serasi.png',
+    screenshot: '/portfolio/Serasi.webp',
   },
   {
     title: 'Website Catalog Brand Clothing',
     description: 'Tampilkan koleksi fashion streetwear dengan katalog interaktif untuk brand clothing modern.',
     route: 'https://doudletfasy.id/',
     category: 'Fashion Catalog',
-    screenshot: '/portfolio/Clothing.png',
+    screenshot: '/portfolio/Clothing.webp',
   },
   {
     title: 'Website Rental Mobil',
     description: 'Permudah pelanggan menemukan dan memesan layanan rental mobil secara online dengan informasi armada lengkap.',
     route: 'https://gibranrentcar.com/',
     category: 'Rental Transportasi',
-    screenshot: '/portfolio/Rental.png',
+    screenshot: '/portfolio/Rental.webp',
   },
   {
     title: 'Website Jasa Ahli Kunci',
     description: 'Solusi ahli kunci profesional 24/7 untuk kebutuhan mobil dan rumah dengan layanan darurat cepat.',
     route: 'https://ahlikunciakkhay.com/',
     category: 'Jasa Layanan',
-    screenshot: '/portfolio/ahliKunci.png',
+    screenshot: '/portfolio/ahliKunci.webp',
   }
   
 ];
